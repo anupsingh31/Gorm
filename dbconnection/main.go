71d9c3a7bc0ad943851265dca1db72a574029f10
Anupam Kumar Singh <48378178+anupsingh31@users.noreply.github.com>
2021-10-06 18:17:27 +0530

    Add files via upload

--- dbconnection/main.go
+++ dbconnection/main.go
@@ -35,27 +35,55 @@ func main() {
 	fmt.Println("Enter true or false customer is IsMale:")
 	fmt.Scanln(&ismale)
 	c := customer.New(fname, lname, age, &ismale)
-	fmt.Println("Enter the Item name which u want to buy:")
-	fmt.Scanln(&itemName)
-	fmt.Println("Enter the Item Discription:")
-	scanner := bufio.NewScanner(os.Stdin)
-	if scanner.Scan() {
-		itemDesc = scanner.Text()
-	}
-	fmt.Println("Enter the number of qunatity u want to buy:")
-	fmt.Scanln(&quantity)
-	fmt.Println("Enter cost of per unit of  ", itemName)
-	fmt.Scanln(&costPerUnit)
-	fmt.Println("Enter true or false if u paid total cost")
-	fmt.Scanln(&isPaid)
-	o := order.New(c.ID, itemName, itemDesc, quantity, costPerUnit, &isPaid)
-	db.AutoMigrate(&customer.Customer{}, &order.Order{})
+	db.AutoMigrate(&customer.Customer{})
 	db.Create(c)
-	db.Create(o)
-	inVoice(*c, *o)
+	var o *order.Order
+	for {
+		var flag bool
+		fmt.Println("Dear Customer: \n Enter 1 for order \n Enter 0 for checkout")
+		fmt.Scanln(&flag)
+		if flag {
+			fmt.Println("Enter the Item name which u want to buy:")
+			fmt.Scanln(&itemName)
+			fmt.Println("Enter the Item Discription:")
+			scanner := bufio.NewScanner(os.Stdin)
+			if scanner.Scan() {
+				itemDesc = scanner.Text()
+			}
+			fmt.Println("Enter the number of qunatity u want to buy:")
+			fmt.Scanln(&quantity)
+			fmt.Println("Enter cost of per unit of  ", itemName)
+			fmt.Scanln(&costPerUnit)
+			fmt.Println("Enter true or false if u paid total cost")
+			fmt.Scanln(&isPaid)
+			o = order.New(c.ID, itemName, itemDesc, quantity, costPerUnit, &isPaid)
+			db.AutoMigrate(&order.Order{})
+			db.Create(o)
+		} else {
+			inVoice(*c, db)
+			break
+		}
+	}
+	// fmt.Println("Enter the Item name which u want to buy:")
+	// fmt.Scanln(&itemName)
+	// fmt.Println("Enter the Item Discription:")
+	// scanner := bufio.NewScanner(os.Stdin)
+	// if scanner.Scan() {
+	// 	itemDesc = scanner.Text()
+	// }
+	// fmt.Println("Enter the number of qunatity u want to buy:")
+	// fmt.Scanln(&quantity)
+	// fmt.Println("Enter cost of per unit of  ", itemName)
+	// fmt.Scanln(&costPerUnit)
+	// fmt.Println("Enter true or false if u paid total cost")
+	// fmt.Scanln(&isPaid)
+	// o := order.New(c.ID, itemName, itemDesc, quantity, costPerUnit, &isPaid)
+	// db.AutoMigrate(&order.Order{})
+	// db.Create(o)
+	// inVoice(*c, *o)
 }
 
-func inVoice(c customer.Customer, o order.Order) {
+func inVoice(c customer.Customer, db *gorm.DB) {
 	cl := exec.Command("cmd", "/c", "cls")
 	cl.Stdout = os.Stdout
 	cl.Run()
@@ -65,14 +93,40 @@ func inVoice(c customer.Customer, o order.Order) {
 	fmt.Println("IsMale  :    ", *c.IsMale)
 	fmt.Println("\n \n")
 	fmt.Println("----------------------------------------------------------")
-	fmt.Println("Item Name                 |       ", o.ItemName)
-	fmt.Println("Item Description          |       ", o.ItemDesc)
-	fmt.Println("Qantity                   |       ", o.Quantity)
-	fmt.Println("Per Unit Cost             |       ", o.CostPerUnit)
-	fmt.Println("IsPaid                    |       ", *o.IsPaid)
-	fmt.Println("\n \n \n")
+	fmt.Println("Item name\t\t| Item Desc\t\t| Quantity\t\t| Cost/unit\t\t| ")
+	var total float64 = 0
+	rows, err := db.DB().Query("SELECT item_name,item_desc,quantity,cost_per_unit,is_paid FROM orders WHERE customer_id = ?", c.ID)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var itemName, itemDesc string
+		var quantity int
+		var costPerUnit float64
+		var isPaid bool
+		err = rows.Scan(&itemName, &itemDesc, &quantity, &costPerUnit, &isPaid)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(itemName, "\t| ", itemDesc, "\t|", quantity, "\t|", costPerUnit, "\t|")
+		total += float64(quantity) * costPerUnit
+	}
 	fmt.Println("----------------------------------------------------------")
-	fmt.Println("                Total Cost|       ", o.CostPerUnit*float64(o.Quantity))
+	fmt.Println("\t\t    \t\t     Total Cost|       ", total)
 	fmt.Println("----------------------------------------------------------")
+
+	if err != nil {
+		panic(err)
+	}
+	// fmt.Println("Item Name                 |       ", o)
+	// fmt.Println("Item Description          |       ", o.ItemDesc)
+	// fmt.Println("Qantity                   |       ", o.Quantity)
+	// fmt.Println("Per Unit Cost             |       ", o.CostPerUnit)
+	// fmt.Println("IsPaid                    |       ", *o.IsPaid)
+	// fmt.Println("\n \n \n")
+	// fmt.Println("----------------------------------------------------------")
+	// fmt.Println("                Total Cost|       ", o.CostPerUnit*float64(o.Quantity))
+	// fmt.Println("----------------------------------------------------------")
 
 }
